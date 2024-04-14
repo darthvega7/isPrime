46bcc4a16ca41dd9056b0b0c926e9fe8afa7d45d
darthvega7 <50564378+darthvega7@users.noreply.github.com>
2024-04-14 17:14:40 -0500

    v0.2 - 04/14/2024
    Checking up to Square Root of number instead of Number / 2

--- Main.java
+++ Main.java
@@ -4,14 +4,16 @@ public class Main {
     public static void checkPrime(int num){
         int divNum = 2;
         boolean isDiv = false;
-        for(int x = 2; x < (num / 2 + 1); x++) {
-            if(num % x == 0) {
-                isDiv = true;
-                divNum = x;
-                break;
+        if(num > 2) {
+            for (int x = 2; x < Math.ceil(Math.sqrt(num)) + 1; x++) {
+                if (num % x == 0) {
+                    isDiv = true;
+                    divNum = x;
+                    break;
+                }
             }
         }
-        if(isDiv) {
+        if (isDiv) {
             System.out.println("Number is NOT prime. Divisible by " + divNum);
         } else {
             System.out.println("Number IS Prime");
